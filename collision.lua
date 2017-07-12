c425e12babf22543a774dfaa71c878fdab10d291
jordan4ibanez <jordan4ibanez@gmail.com>
2017-07-12 02:23:42 -0400

    Collision detect outside chunk 0,0

--- collision.lua
+++ collision.lua
@@ -23,13 +23,28 @@ function collision(oldposx,oldposy)
 			local chunkerx = 0
 			local chunkery = 0
 			
-			if squarex < 1 or squarex > map_max then
-				print(squarex)
+			if squarex < 1 then
+				chunkerx = -1
+				squarex = map_max
+				print("adjusting x -1")
+			elseif squarex > map_max then
+				chunkerx = 1
+				squarex = 1
+				print("adjusting x +1")
 			end
-			
-			
+			if squarey < 1 then
+				chunkery = 1
+				squarey = map_max
+				print("adjusting y +1")
+			elseif squarey > map_max then
+				chunkery = -1
+				squarey = 1
+				print("adjusting y -1")
+			end
+			print(chunkerx, chunkery, "|", squarex,squarey)
+			print( loaded_chunks[chunkerx][chunkery][squarex][squarey]["block"])
 			--if (squarex1 > map_max or squarex1 <= 0) or (squarey1 > map_max or squarey1 <= 0) or ore[loaded_chunks[0][0][squarex1][squarey1]["block"]]["collide"] ~= false then
-			if (squarex <= map_max and squarex >= 1) and (squarey <= map_max and squarey >= 1) and ore[loaded_chunks[0][0][squarex][squarey]["block"]]["collide"] ~= false then
+			if ore[loaded_chunks[chunkerx][chunkery][squarex][squarey]["block"]]["collide"] ~= false then
 				player.playery = oldposy
 				if r == 2 then
 					player.on_block = true
@@ -52,8 +67,32 @@ function collision(oldposx,oldposy)
 		for r = 1,2 do
 			local squarex = math.floor(player.playerx+xer[q])
 			local squarey = math.floor(player.playery+yer[r])
+			
+			--use this to detect outside chunk 00
+			local chunkerx = 0
+			local chunkery = 0
+			
+			if squarex < 1 then
+				chunkerx = -1
+				squarex = map_max
+				print("adjusting x -1")
+			elseif squarex > map_max then
+				chunkerx = 1
+				squarex = 1
+				print("adjusting x +1")
+			end
+			if squarey < 1 then
+				chunkery = 1
+				squarey = map_max
+				print("adjusting y +1")
+			elseif squarey > map_max then
+				chunkery = -1
+				squarey = 1
+				print("adjusting y -1")
+			end
+			
 			--if (squarex1 > map_max or squarex1 <= 0) or (squarey1 > map_max or squarey1 <= 0) or ore[loaded_chunks[0][0][squarex1][squarey1]["block"]]["collide"] ~= false then
-			if (squarex <= map_max and squarex >= 1) and (squarey <= map_max and squarey >= 1) and ore[loaded_chunks[0][0][squarex][squarey]["block"]]["collide"] ~= false then
+			if ore[loaded_chunks[chunkerx][chunkery][squarex][squarey]["block"]]["collide"] ~= false then
 				player.inertiax = 0
 				player.playerx = oldposx
 				--print("stopping x inertia and pos")
